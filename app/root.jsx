ab544e649c3a2a1219dac4e28a99b30f588348ba
Jennifer González <jenniferjacqueline936@gmail.com>
2023-05-28 21:13:08 -0600

    Adding the styles to use

--- app/root.jsx
+++ app/root.jsx
@@ -1,3 +1,49 @@
+import {
+    Meta,
+    Links
+} from '@remix-run/react'
+import styles from './styles/index.css';
+
+export function meta(){
+    return [
+        {
+            charset: 'utf-8'
+        },
+        {
+            title: 'GuitarLA - Remix'
+        },
+        {
+            viewport: "width=device-width,initial-scale=1"
+        }
+    ]
+}
+
+export function links(){
+    return [
+        {
+            rel: 'stylesheet',
+            href: 'https://necolas.github.io/normalize.css/8.0.1/normalize.css'
+        },
+        {
+            rel: 'preconect',
+            href: 'https://fonts.googleapis.com'
+        },
+        {
+            rel: 'preconnect',
+            href: 'https://fonts.gstatic.com',
+            crossOrigin: 'true'
+        },
+        {
+            rel: 'stylesheet',
+            href: 'https://fonts.googleapis.com/css2?family=Outfit:wght@400;700;900&display=swap'
+        },
+        {
+            rel: 'stylesheet',
+            href: styles
+        }
+    ]
+}
+
 export default function App(){
     return(
         <Document>
@@ -10,7 +56,8 @@ function Document({children}){
     return (
         <html lang="es">
             <head>
-                <title>GuitarLA - Remix</title>
+                <Meta />
+                <Links />              
             </head>
             <body>
                 {children}
